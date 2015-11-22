ca6e03a11053d3319c87ad3e603cfdf500e6d027
izelichenko <isabella.zelichenko@gmail.com>
2015-11-22 21:26:34 +0900

    Create Vertex.java

---
+++ Vertex.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+/**
+ * Vertex.java Implementation of a class called "Vertex" which contains data and
+ * an ArrayList of neighboring vertices according to a database
+ * 
+ * @author Isabella Zelichenko and Alan Wang
+ * 
+ */
